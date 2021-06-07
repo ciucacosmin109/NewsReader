ee0fb8fead30de2f0648a4ebd24d36c7970f563d
Marius Cosmin Ciuca <ciucacosmin109@gmail.com>
2021-06-07 16:12:26 +0300

    Added a placeholder for the articles without an image

--- app/src/main/java/com/marius/newsreader/view/bindings/ImageBinding.java
+++ app/src/main/java/com/marius/newsreader/view/bindings/ImageBinding.java
@@ -4,6 +4,10 @@ import android.graphics.Bitmap;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.marius.newsreader.R;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.Nullable;
@@ -13,8 +17,15 @@ public class ImageBinding {
 
     @BindingAdapter({"imageUrl"})
     public static void setImageUrl(ImageView imageView, @Nullable String url) {
+        RequestOptions options = new RequestOptions()
+                .centerCrop()
+                .error(R.drawable.ic_outline_text_snippet_24)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .priority(Priority.HIGH);
+
         Glide.with(imageView.getContext())
                 .load(url)
+                .apply(options)
                 .into(imageView);
     }
 }
